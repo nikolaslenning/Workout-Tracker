72df8e3b2b4055f91289a255680ada972e8dab8b
Nikolas Lenning <nikolaslenning@gmail.com>
2020-11-13 15:55:25 -0700

    get route attempt

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,23 +4,31 @@ const Cardio = require("../models/cardio.js");
 
 router.get("/api/workouts", (req, res) => {
 
-    Resistance.find({})
-        .sort({ day: -1 })
-        .then(dbResistance => {
-            res.json(dbResistance);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+    Resistance.find({}, (error, data) => {
+        if (error) {
+            res.send(error);
+        } else {
+            res.json(data);
+        }
+    });
 
-    Cardio.find({})
-        .sort({ day: -1 })
-        .then(dbCardio => {
-            res.json(dbCardio);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+    // Resistance.find({})
+    //     .sort({ day: -1 })
+    //     .then(dbResistance => {
+    //         res.json(dbResistance);
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     });
+
+    // Cardio.find({})
+    //         .sort({ day: -1 })
+    //         .then(dbCardio => {
+    //             res.json(dbCardio);
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //         });
 });
 
 module.exports = router;
