f86f64d375263be8f2b58673caafc727a1f3ea3e
Nikolas Lenning <nikolaslenning@gmail.com>
2020-11-13 16:11:38 -0700

    required in HTML routes

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
