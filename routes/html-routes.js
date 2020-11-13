df9970a21ad686410d25043fbd8151f4a409baff
Nikolas Lenning <nikolaslenning@gmail.com>
2020-11-13 16:10:29 -0700

    added root route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,6 +2,10 @@ var path = require("path");
 const router = require("express").Router();
 
 
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
