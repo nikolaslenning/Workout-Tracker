f23e44536461758b2f7095d5580c6e4cee60a3ae
Nikolas Lenning <nikolaslenning@gmail.com>
2020-11-15 13:17:32 -0700

    added router.put & attempting update

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,31 +5,113 @@ const Cardio = require("../models/cardio.js");
 
 router.get("/api/workouts", (req, res) => {
 
-    Resistance.find({}, (error, data) => {
-        if (error) {
-            res.send(error);
-        } else {
-            res.json(data);
+  Resistance.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
+  });
+
+  // Resistance.find({})
+  //     .sort({ day: -1 })
+  //     .then(dbResistance => {
+  //         res.json(dbResistance);
+  //     })
+  //     .catch(err => {
+  //         console.log(err)
+  //     });
+
+  // Cardio.find({})
+  //         .sort({ day: -1 })
+  //         .then(dbCardio => {
+  //             res.json(dbCardio);
+  //         })
+  //         .catch(err => {
+  //             console.log(err)
+  //         });
+});
+function logIt(body) {
+  console.log(body);
+  return body
+}
+
+router.post("/api/workouts", async function (req, res) {
+  // console.log("body body body body")
+  // console.log(req.body);
+  // var bodyValue = await logIt (body) 
+});
+// router.post("/api/workouts", async function (req, res) {
+//   console.log("body body body body")
+//   console.log(req);
+//   // var bodyValue = await logIt (body) 
+// });
+
+router.put("/api/workouts/:id", (req, res) => {
+  let body = req.body
+  console.log("req.params")
+  console.log(req.params)
+  // console.log(req)
+  
+  if (body.type === "cardio") {
+    console.log('cardio hit')
+    Cardio.updateOne(
+      {
+        // _id: mongojs.ObjectId(params.id)
+      },
+      {
+        $set: {
+          type: body.type,
+          name: body.name,
+          distance: body.distance,
+          duration: body.duration
         }
-    });
+      },
 
-    // Resistance.find({})
-    //     .sort({ day: -1 })
-    //     .then(dbResistance => {
-    //         res.json(dbResistance);
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     });
+      (error, edited) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+        } else {
+          console.log(edited);
+          res.send(edited);
+        }
+      }
+    );
+  };
+  
+  if (body.type === "resistance") {
+    console.log('resistance hit')
+    Resistance.updateOne(
+      {
+        // _id: mongojs.ObjectId(params.id)
+      },
+      {
+        $set: {
+          type: body.type,
+          name: body.name,
+          weight: body.weight,
+          sets: body.sets,
+          reps: body.reps,
+          duration: body.duration
+        }
+      },
 
-    // Cardio.find({})
-    //         .sort({ day: -1 })
-    //         .then(dbCardio => {
-    //             res.json(dbCardio);
-    //         })
-    //         .catch(err => {
-    //             console.log(err)
-    //         });
+      (error, edited) => {
+        if (error) {
+          // console.log("error");
+          // console.log(error);
+          res.send(error);
+        } else {
+          // console.log("edited");
+          // console.log(edited);
+          res.send(edited);
+        }
+      }
+    );
+  };
+
+
 });
 
 
--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true 
 });
 
 // routes
