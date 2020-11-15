45a7c7811c597a680c8e9a7ee74a9056bc91b6ec
Nikolas Lenning <nikolaslenning@gmail.com>
2020-11-15 13:46:59 -0700

    added mongoose connection variables

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
