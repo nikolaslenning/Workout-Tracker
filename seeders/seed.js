cf0f7bf14edad001deda032238eec8d6434268c4
Nikolas Lenning <nikolaslenning@gmail.com>
2020-11-18 12:32:57 -0700

    updated mongoose.connect

--- seeders/seed.js
+++ seeders/seed.js
@@ -3,6 +3,8 @@ let db = require("../models");
 
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
